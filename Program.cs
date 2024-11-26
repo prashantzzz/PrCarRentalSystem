c54bd50f2e04a0124cb866447195ae5b9bec8e81
Prashantzzz <prashant2003prashant@gmail.com>
2024-11-26 11:50:21 +0530

    updated jsonoptions in program.cs

--- Program.cs
+++ Program.cs
@@ -11,12 +11,15 @@ using PrCarRentalSystem.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
 builder.Services.AddControllers(options =>
 {
     options.Filters.Add<ValidationFilter>();
+})
+.AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
 });
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
 builder.Services.AddScoped<ICarRepository, CarRepository>();
